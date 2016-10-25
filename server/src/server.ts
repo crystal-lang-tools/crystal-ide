1caf9f19ceff51cf7dd60c11b854a2d812f62c74
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2016-10-25 18:53:43 -0500

    Allow spaces and special characters on path
    Support Check filepath with spaces or special characters like `/home/user/Project/Español Veragüense/español.cr`

--- server/src/server.ts
+++ server/src/server.ts
@@ -68,13 +68,13 @@ connection.onDidChangeConfiguration((change) => {
 });
 
 function fileUriToPath(uri: string) : string {
-	return uri.replace("file://", "");
+	return decodeURI(uri.replace("file://", ""));
 }
 
 function validateFile(file: FileChanged): void {
 	let exec = require('child_process').exec;
 	let diagnostics: Diagnostic[] = [];
-	exec("crystal build --no-color --no-codegen -f json --release " + fileUriToPath(file.uri), (err, response) => {
+	exec("crystal build --no-color --no-codegen -f json --release \"" + fileUriToPath(file.uri) + "\"", (err, response) => {
 		if (response) {
 			let results = JSON.parse(response);
 			let length = Math.min(maxNumberOfProblems, results.length);
